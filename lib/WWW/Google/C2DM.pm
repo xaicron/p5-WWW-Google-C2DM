f5397a91f252e92441bffed71d30b7da76ba7f9a
xaicron <xaicron@gmail.com>
2011-12-27 19:59:20 +0900

    first commit

--- lib/WWW/Google/C2DM.pm
+++ lib/WWW/Google/C2DM.pm
@@ -2,8 +2,71 @@ package WWW::Google::C2DM;
 
 use strict;
 use warnings;
+use Carp qw(croak);
+use HTTP::Request;
+use LWP::UserAgent;
+use LWP::protocol::https;
+
+use WWW::Google::C2DM::Response;
+
 use 5.008_001;
 our $VERSION = '0.01';
+
+our $URL = 'https://android.apis.google.com/c2dm/send';
+$ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0; # suppress certificate verify failed
+
+sub new {
+    my ($class, %args) = @_;
+    croak "Usage: $class->new(auth_token => \$auth_token)" unless $args{auth_token};
+    $args{ua} ||= LWP::UserAgent->new(agent => __PACKAGE__.' / '.$VERSION);
+    bless { %args }, $class;
+}
+
+sub send {
+    my ($self, %args) = @_;
+    croak 'Usage: $self->send(registration_id => $reg_id, collapse_key => $collapse_key)'
+        unless $args{registration_id} && $args{collapse_key};
+
+    if (my $data = delete $args{data}) {
+        croak 'data parameter must be HASHREF' unless ref $data eq 'HASH';
+        map { $args{"data.$_"} = $data->{$_} } keys %$data;
+    }
+
+    my $req = HTTP::Request->new(POST => $URL);
+    $req->header('Content-Type' => 'application/x-www-form-urlencoded');
+    $req->header(Authorization  => 'GoogleLogin auth='.$self->{auth_token});
+    $req->content(join '&', map { $_.'='.$args{$_} } keys %args);
+
+    my $http_response = $self->{ua}->request($req);
+
+    my $result;
+    if ($http_response->code == 200) {
+        my $content = $http_response->content;
+        my $params = { map { split '=', $_, 2 } split /\n/, $content };
+        if ($params->{Error}) {
+            $result = WWW::Google::C2DM::Response->new(
+                is_success    => 0,
+                error_code    => $params->{Error},
+                http_response => $http_response,
+            );
+        }
+        else {
+            $result = WWW::Google::C2DM::Response->new(
+                is_success    => 1,
+                http_response => $http_response,
+                params        => $params,
+            );
+        }
+    }
+    else {
+        $result = WWW::Google::C2DM::Response->new(
+            is_success    => 0,
+            http_response => $http_response,
+        );
+    }
+
+    return $result;
+}
 
 1;
 __END__
---
+++ lib/WWW/Google/C2DM/Response.pm
@@ -0,0 +1,150 @@
+package WWW::Google::C2DM::Response;
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %args) = @_;
+    $args{params} ||= {};
+    bless { %args }, $class;
+}
+
+sub is_success {
+    $_[0]->{is_success} ? 1 : 0;
+}
+
+sub has_error {
+    !$_[0]->is_success;
+}
+
+sub code {
+    $_[0]->http_response->{code};
+}
+
+sub message {
+    $_[0]->http_response->{message};
+}
+
+sub error_code {
+    $_[0]->{error_code} || '';
+}
+
+sub status_line {
+    my $self = shift;
+    sprintf '%d %s', $self->code, $self->message;
+}
+
+sub http_response {
+    $_[0]->{http_response};
+}
+
+sub params {
+    $_[0]->{params};
+}
+
+sub id {
+    $_[0]->params->{id};
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+WWW::Google::C2DM::Response - Response Object
+
+=head1 SYNOPSIS
+
+  my $res = WWW::Google::C2DM::Response->new(
+      is_success    => 1,
+      code          => 200,
+      message       => 'OK',
+      http_response => $http_response_object,
+      params        => { ... },
+  );
+
+=head1 DESCRIPTION
+
+WWW::Google::C2DM::Response is a WWW::Google::C2DM intarnal class.
+
+=head1 METHODS
+
+=over
+
+=item new(%args)
+
+=item is_success()
+
+  $res->is_success ? 1 : 0;
+
+=item has_error()
+
+  $res->has_error ? 1 : 0;
+
+=item code()
+
+HTTP Response code.
+
+  say $res->code;
+
+=item message()
+
+HTTP Response message.
+
+  say $res->message;
+
+=item status_line()
+
+C<< code >> and C<< message >>
+
+  say $res->status_line; # eq say $res->code, ' ', $res->message;
+
+=item http_response()
+
+Original HTTP Response object.
+
+  my $http_response = $res->http_response;
+  say $http_response->as_string;
+
+=item error_code()
+
+C2DM error code. SEE ALSO L<< http://code.google.com/intl/ja/android/c2dm/#push >>
+
+  if ($res->error_code eq 'QuotaExceeded') {
+     ...
+  }
+
+=item id()
+
+Response id param.
+
+  say $res->id;
+
+=item params()
+
+Response parameters in HASHREF.
+
+  say $res->params->{id};
+
+=back
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2011 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
