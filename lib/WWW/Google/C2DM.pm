db7989cc6ba3d6a693699bbb8fce967dc565b325
xaicron <xaicron@gmail.com>
2012-03-12 18:01:45 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 86a5397..26275d9 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension WWW::Google::C2DM
    
    +0.04  Mon Mar 12 17:57:31 2012
    +    - fixed problem that broken on older LWP::UserAgent
    +
     0.03  Wed Feb 15 01:23:45 2012
         - added is_error method on WWW::Google::C2DM::Response

--- lib/WWW/Google/C2DM.pm
+++ lib/WWW/Google/C2DM.pm
@@ -10,7 +10,7 @@ use LWP::Protocol::https;
 use WWW::Google::C2DM::Response;
 
 use 5.008_001;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 our $URL = 'https://android.apis.google.com/c2dm/send';
 
